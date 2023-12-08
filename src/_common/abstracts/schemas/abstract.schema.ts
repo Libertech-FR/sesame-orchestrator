ac6ccd18dd9acf9e71ada46e852b06925783715f
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-12-08 17:15:43 +0100

    Refactor import statement in abstract.schema.ts

--- src/_common/abstracts/schemas/abstract.schema.ts
+++ src/_common/abstracts/schemas/abstract.schema.ts
@@ -1,9 +1,6 @@
 import { Prop } from '@nestjs/mongoose';
 import { Types, Document } from 'mongoose';
-import {
-  MetadataPart,
-  MetadataPartSchema,
-} from '~/_common/abstracts/schemas/parts/metadata.part.schema';
+import { MetadataPart, MetadataPartSchema } from '~/_common/abstracts/schemas/parts/metadata.part.schema';
 
 export abstract class AbstractSchema extends Document {
   public readonly _id: Types.ObjectId | any // eslint-disable-line

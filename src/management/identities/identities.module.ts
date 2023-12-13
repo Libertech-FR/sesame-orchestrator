9dd689fa24f1636d73dbe81655b35e70be64b787
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2023-12-13 17:34:07 +0100

    Add pre-validation and pre-save hooks to identities module

--- src/management/identities/identities.module.ts
+++ src/management/identities/identities.module.ts
@@ -5,6 +5,7 @@ import { IdentitiesService } from './identities.service';
 import { IdentitiesController } from './identities.controller';
 import { IdentitiesValidationService } from './validations/identities.validation.service';
 import { IdentitiesValidationModule } from './validations/identities.validation.module';
+import { IdentityState } from './_enums/states.enum';
 
 @Module({
   imports: [
@@ -15,12 +16,31 @@ import { IdentitiesValidationModule } from './validations/identities.validation.
         inject: [IdentitiesValidationService],
         useFactory: (validationService: IdentitiesValidationService) => {
           const schema = IdentitiesSchema;
+          // Pre validation hook
+          // This hook is used to validate the additionalFields
+          // If the validation fails and the state is TO_CREATE, the state is set to TO_COMPLETE
+          // Else the error is thrown
           schema.pre('validate', async function (next) {
-            Logger.log('additionalFields validation start');
-            await validationService.validate(this.additionalFields);
-            Logger.log('additionalFields validation done');
+            try {
+              Logger.log(`additionalFields validation start for ${this.inetOrgPerson.cn}  `);
+              await validationService.validate(this.additionalFields);
+              Logger.log(`additionalFields validation end for ${this.inetOrgPerson.cn}  `);
+            } catch (error) {
+              Logger.error(`additionalFields validation error for ${this.inetOrgPerson.cn}  `, error);
+              if (this.state === IdentityState.TO_CREATE) this.state = IdentityState.TO_COMPLETE;
+              else throw error;
+            }
             next();
           });
+
+          // Pre save hook
+          // This hook is used to set the state to TO_SYNCED if the state is TO_CREATE
+          schema.pre('save', async function (next) {
+            console.log('pre save');
+            if (this.state === IdentityState.TO_CREATE) this.state = IdentityState.TO_SYNCED;
+            next();
+          });
+
           return schema;
         },
       },

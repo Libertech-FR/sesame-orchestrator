83d79f7d3225354acf0d88302544ae2738f2c37d
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2024-01-09 17:42:59 +0100

    Refactor createMockModel function in mock.model.ts

--- src/_common/testsHelpers/mock.model.ts
+++ src/_common/testsHelpers/mock.model.ts
@@ -1,4 +1,10 @@
 import { Model } from 'mongoose';
+/**
+ * Mock a model
+ * @param model
+ * @param stub
+ * @param updatedStub
+ */
 
 export function createMockModel<T>(model: Model<T>, stub, updatedStub?): Model<T> {
   model.countDocuments = jest.fn().mockImplementationOnce(() => ({
@@ -18,16 +24,18 @@ export function createMockModel<T>(model: Model<T>, stub, updatedStub?): Model<T
   }));
 
   model.findByIdAndUpdate = jest.fn().mockImplementationOnce(() => ({
-    exec: jest.fn().mockResolvedValueOnce(updatedStub),
+    exec: jest.fn().mockResolvedValueOnce(updatedStub ? updatedStub : stub),
   }));
 
   model.findByIdAndDelete = jest.fn().mockImplementationOnce(() => ({
     exec: jest.fn().mockResolvedValueOnce(stub),
   }));
 
-  model.prototype = jest.fn().mockImplementationOnce(() => ({
-    save: jest.fn().mockResolvedValueOnce(stub),
-  }));
+  if (!model.prototype.save) {
+    model.prototype.save = jest.fn();
+  }
+
+  jest.spyOn(model.prototype, 'save').mockImplementationOnce(() => Promise.resolve(stub));
 
   return model;
 }

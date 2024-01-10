1f4081e53a5361a4d4928c50749bb946c5f8c5b9
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2024-01-10 10:57:28 +0100

    Refactor model mocking in tests

--- src/_common/tests/mock.model.ts
+++ src/_common/tests/mock.model.ts
@@ -13,33 +13,45 @@ export function createMockModel<T>(model: Model<T>, stub, updatedStub?, shouldTh
     exec: shouldThrowError ? jest.fn().mockResolvedValueOnce(1) : jest.fn().mockResolvedValueOnce(0),
   }));
 
-  model.find = jest.fn().mockImplementationOnce(() => ({
-    exec: shouldThrowError ? jest.fn().mockResolvedValueOnce([stub]) : jest.fn().mockResolvedValueOnce([]),
-  }));
+  model.find = shouldThrowError
+    ? jest.fn().mockImplementationOnce(() => {
+        throw new NotFoundException();
+      })
+    : jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValueOnce([stub]),
+      }));
 
-  model.findOne = jest.fn().mockImplementationOnce(() => ({
-    exec: shouldThrowError
-      ? jest.fn().mockResolvedValueOnce(stub)
-      : jest.fn(() => Promise.reject(new NotFoundException())),
-  }));
+  model.findOne = shouldThrowError
+    ? jest.fn().mockImplementationOnce(() => {
+        throw new NotFoundException();
+      })
+    : jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValueOnce(stub),
+      }));
 
-  model.findById = jest.fn().mockImplementationOnce(() => ({
-    exec: shouldThrowError
-      ? jest.fn().mockResolvedValueOnce(stub)
-      : jest.fn(() => Promise.reject(new NotFoundException())),
-  }));
+  model.findById = shouldThrowError
+    ? jest.fn().mockImplementationOnce(() => {
+        throw new NotFoundException();
+      })
+    : jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValueOnce(stub),
+      }));
 
-  model.findByIdAndUpdate = jest.fn().mockImplementationOnce(() => ({
-    exec: shouldThrowError
-      ? jest.fn().mockResolvedValueOnce(updatedStub ? updatedStub : stub)
-      : jest.fn(() => Promise.reject(new NotFoundException())),
-  }));
+  model.findByIdAndUpdate = shouldThrowError
+    ? jest.fn().mockImplementationOnce(() => {
+        throw new NotFoundException();
+      })
+    : jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValueOnce(updatedStub ? updatedStub : stub),
+      }));
 
-  model.findByIdAndDelete = jest.fn().mockImplementationOnce(() => ({
-    exec: shouldThrowError
-      ? jest.fn().mockResolvedValueOnce(stub)
-      : jest.fn(() => Promise.reject(new NotFoundException())),
-  }));
+  model.findByIdAndDelete = shouldThrowError
+    ? jest.fn().mockImplementationOnce(() => {
+        throw new NotFoundException();
+      })
+    : jest.fn().mockImplementationOnce(() => ({
+        exec: jest.fn().mockResolvedValueOnce(stub),
+      }));
 
   if (!model.prototype.save) {
     model.prototype.save = shouldThrowError ? jest.fn() : jest.fn(() => Promise.reject(new Error('Error')));

7839153d0abbcb6186dc65ab4d8149e2ce3c377f
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2024-01-23 16:24:29 +0100

    Refactor form and logger schemas

--- src/core/form/_dto/form.dto.ts
+++ src/core/form/_dto/form.dto.ts
@@ -1,54 +1,49 @@
-import { MixedValue } from './../../../_common/types/mixed-value.type'
-import { Mixed } from 'mongoose'
-import { ApiProperty, PartialType } from '@nestjs/swagger'
-import { IsString, IsEnum, IsObject, ValidateNested, IsOptional, IsMongoId } from 'class-validator'
-import { ObjectId } from 'mongodb'
-import { MixedValue } from '~/_common/types/mixed-value.type'
-import { FormTypeList, FormTypes } from '../_enum/types'
-import { MetadataDto } from '~/_common/abstracts/dto/metadata.dto'
-import { forwardRef } from '@nestjs/common'
-import { Type } from 'class-transformer'
-import { FormSectionDto } from './parts/section.dto'
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsString, IsEnum, IsObject, ValidateNested, IsOptional } from 'class-validator';
+import { MixedValue } from '~/_common/types/mixed-value.type';
+import { FormTypeList, FormTypes } from '../_enum/types';
+import { MetadataDto } from '~/_common/abstracts/dto/metadata.dto';
+import { Type } from 'class-transformer';
+import { FormSectionDto } from './parts/section.dto';
 
 export class FormCreateDto extends MetadataDto {
   @IsString()
   @ApiProperty()
-  title: string
+  title: string;
 
   @IsString()
   @IsOptional()
   @ApiProperty({ required: false })
-  description?: string
+  description?: string;
 
   @IsEnum(FormTypeList)
   @ApiProperty({ enum: FormTypeList })
-  type: FormTypes
+  type: FormTypes;
 
   @IsObject()
   @IsOptional()
   @ApiProperty({ required: false })
-  defaultValues: { [key: string]: MixedValue }
+  defaultValues: { [key: string]: MixedValue };
 
   @ValidateNested({ each: true })
   @Type(() => FormSectionDto)
   // @ApiProperty({ type: () => FormSectionDto, isArray: true })
-  sections: { [key: string]: FormSectionDto }
+  sections: { [key: string]: FormSectionDto };
 
   @IsString()
   @ApiProperty()
-  submitButtonText: string
+  submitButtonText: string;
 
   @IsString()
   @ApiProperty()
-  submitApiUrl: string
+  submitApiUrl: string;
 
   @IsObject()
   @IsOptional()
   @ApiProperty({ required: false })
-  redirectUrl?: string
+  redirectUrl?: string;
 }
 
 export class FormDto extends FormCreateDto {}
-
 
 export class FormUpdateDto extends PartialType(FormDto) {}
--- src/core/logger/schemas/logger.schema.ts
+++ src/core/logger/schemas/logger.schema.ts
@@ -1,10 +1,10 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
-import { Abstract } from '~/_common/abstracts/abstract/schemas/abstract.schema'
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { AbstractSchema } from '~/_common/abstracts/schemas/abstract.schema';
 
-export type LoggerDocument = Logger & Document
+export type LoggerDocument = Logger & Document;
 
 @Schema({ versionKey: false })
 export class Logger extends AbstractSchema {}
 
-export const LoggerSchema = SchemaFactory.createForClass(Logger)
+export const LoggerSchema = SchemaFactory.createForClass(Logger);

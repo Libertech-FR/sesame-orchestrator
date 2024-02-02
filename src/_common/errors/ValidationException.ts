6eb85d9d607bf89fc5f49fb250c57a6b64fcb994
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2024-02-02 16:54:15 +0100

    Refactor ValidationException class and add ValidationExceptionPayload type

--- src/_common/errors/ValidationException.ts
+++ src/_common/errors/ValidationException.ts
@@ -1,13 +1,43 @@
 import { HttpException } from '@nestjs/common';
+import { ValidationExceptionPayload } from '~/_common/types/validation-exception-payload.type';
+
+const DEFAULT_STATUS_CODE = 400;
+const DEFAULT_MESSAGE = 'Validation failed';
+const DEFAULT_VALIDATIONS: Record<string, string> = {};
 
 export class ValidationException extends HttpException {
-  constructor(message, statusCode = 400) {
-    super(message, statusCode);
+  private readonly validations: Record<string, string>;
+
+  constructor(payload: ValidationExceptionPayload) {
+    const { message, validations, statusCode } = payload;
+    super(message || DEFAULT_MESSAGE, statusCode || DEFAULT_STATUS_CODE);
     this.name = this.constructor.name;
-    this.getStatus = () => statusCode;
+    this.validations = validations || DEFAULT_VALIDATIONS;
+  }
+
+  getValidations(): Record<string, string> {
+    return this.validations;
+  }
+
+  getPayload(): ValidationExceptionPayload {
+    return {
+      message: this.message,
+      statusCode: this.getStatus(),
+      validations: this.validations,
+    };
   }
 }
 
-export class ValidationSchemaException extends ValidationException {}
+export class ValidationSchemaException extends ValidationException {
+  constructor(payload: ValidationExceptionPayload) {
+    payload.message = payload.message || 'Wrong schema validation';
+    super(payload);
+  }
+}
 
-export class ValidationConfigException extends ValidationException {}
+export class ValidationConfigException extends ValidationException {
+  constructor(payload: ValidationExceptionPayload) {
+    payload.message = payload.message || 'Wrong config validation';
+    super(payload);
+  }
+}

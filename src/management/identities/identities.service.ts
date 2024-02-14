615a856a38cb658dd9f2a302f382ee1b349ee1bf
Tacx <12997062+tacxou@users.noreply.github.com>
2024-02-14 11:37:02 +0100

    clean up repo

--- src/management/identities/identities.service.ts
+++ src/management/identities/identities.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Identities } from './_schemas/identities.schema';
-import { Model } from 'mongoose';
+import { Document, Model, ModifyResult, Query, QueryOptions, SaveOptions, Types, UpdateQuery } from 'mongoose';
 import { AbstractServiceSchema } from '~/_common/abstracts/abstract.service.schema';
+import { AbstractSchema } from '~/_common/abstracts/schemas/abstract.schema';
 
 @Injectable()
 export class IdentitiesService extends AbstractServiceSchema {
   constructor(@InjectModel(Identities.name) protected _model: Model<Identities>) {
     super();
+  }
+
+
+  public async create<T extends AbstractSchema | Document>(data?: any, options?: SaveOptions): Promise<Document<T, any, T>> {
+    // noinspection UnnecessaryLocalVariableJS
+    const created: Document<T, any, T> = await super.create(data, options);
+    //TODO: add backends service logic here
+    return created;
+  }
+
+  public async update<T extends AbstractSchema | Document>(
+    _id: Types.ObjectId | any,
+    update: UpdateQuery<T>,
+    options?: QueryOptions<T> & { rawResult: true },
+  ): Promise<ModifyResult<Query<T, T, any, T>>> {
+    // noinspection UnnecessaryLocalVariableJS
+    const updated = await super.update(_id, update, options);
+    //TODO: add backends service logic here
+    return updated;
+  }
+
+  public async delete<T extends AbstractSchema | Document>(_id: Types.ObjectId | any, options?: QueryOptions<T> | null | undefined): Promise<Query<T, T, any, T>> {
+    // noinspection UnnecessaryLocalVariableJS
+    const deleted = await super.delete(_id, options);
+    //TODO: add backends service logic here
+    return deleted;
   }
 }
--- src/management/management.module.ts
+++ src/management/management.module.ts
@@ -2,11 +2,10 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { ManagementService } from './management.service';
 import { ManagementController } from './management.controller';
 import { RouterModule } from '@nestjs/core';
-import { PasswdModule } from './passwd/passwd.module';
 import { IdentitiesModule } from './identities/identities.module';
 
 @Module({
-  imports: [PasswdModule, IdentitiesModule],
+  imports: [IdentitiesModule],
   providers: [ManagementService],
   controllers: [ManagementController],
 })

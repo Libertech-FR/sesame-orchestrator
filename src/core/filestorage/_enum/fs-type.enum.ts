4c638f515e8dd913541ffa5ceaaa9e4b62fdc308
Tacx <12997062+tacxou@users.noreply.github.com>
2024-08-01 10:16:32 +0200

    chore: Add FilestorageModule to CoreModule imports

---
+++ src/core/filestorage/_enum/fs-type.enum.ts
@@ -0,0 +1,11 @@
+export enum FsType {
+  DIRECTORY = 'd',
+  FILE = 'f',
+  SYMLINK = 'l',
+}
+
+export const FsTypeList: number[] = Object.keys(FsType)
+  // eslint-disable-next-line
+  .filter((k) => typeof FsType[k as any] === 'string')
+  // eslint-disable-next-line
+  .map((k) => FsType[k as any])
---
+++ src/core/filestorage/filestorage.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Filestorage, FilestorageSchema } from './_schemas/filestorage.schema'
+import { FilestorageService } from './filestorage.service'
+import { FilestorageController } from './filestorage.controller'
+import { TransformersFilestorageService } from '~/core/filestorage/_services/transformers-filestorage.service'
+
+@Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Filestorage.name,
+        useFactory: () => FilestorageSchema,
+      },
+    ]),
+  ],
+  providers: [FilestorageService, TransformersFilestorageService],
+  controllers: [FilestorageController],
+  exports: [FilestorageService],
+})
+export class FilestorageModule { }

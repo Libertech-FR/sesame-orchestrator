084295f92e726dcfe7ecb32330f2a3ea873e84af
Tacx <12997062+tacxou@users.noreply.github.com>
2025-03-03 12:02:22 +0100

    Refactor GithubUpdate interface and update API call to fetch latest releases

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,15 +6,51 @@ import { Public } from './_common/decorators/public.decorator';
 import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { LRUCache } from 'lru-cache';
 
+interface GithubAuthor {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  user_view_type: string;
+  site_admin: boolean;
+}
+
+interface GithubAsset {
+  [key: string]: any;
+}
+
 interface GithubUpdate {
-  name?: string;
-  commit?: {
-    sha?: string;
-    url?: string;
-  };
-  zipball_url?: string;
-  tarball_url?: string;
-  node_id?: string;
+  url: string;
+  assets_url: string;
+  upload_url: string;
+  html_url: string;
+  id: number;
+  author: GithubAuthor;
+  node_id: string;
+  tag_name: string;
+  target_commitish: string;
+  name: string;
+  draft: boolean;
+  prerelease: boolean;
+  created_at: string;
+  published_at: string;
+  assets: GithubAsset[];
+  tarball_url: string;
+  zipball_url: string;
+  body: string;
 }
 
 const storage = new LRUCache({
@@ -47,24 +83,25 @@ export class AppController extends AbstractController {
     @Param('project') project?: string,
     @Query('current') current?: string,
   ): Promise<Response> {
-    let data: GithubUpdate[] | object = {};
+    let data = <GithubUpdate>{};
     // console.log('this.storage', storage.get(project))
     if (storage.has(project)) {
       this.logger.log(`Fetching ${project} tags from cache`);
-      data = storage.get(project) as GithubUpdate[] | object;
+      data = storage.get(project) as GithubUpdate;
     } else {
       this.logger.log(`Fetching ${project} tags`);
-      const update = await fetch(`https://api.github.com/repos/Libertech-FR/${project}/tags`, {
+      const update = await fetch(`https://api.github.com/repos/Libertech-FR/${project}/releases/latest`, {
         signal: AbortSignal.timeout(1000),
       });
       data = await update.json();
+      console.log('update', data)
       storage.set(project, data);
       // console.log('this.storage', storage.get(project))
     }
-    if (!Array.isArray(data)) {
-      throw new BadRequestException(`Invalid data from Github <${JSON.stringify(data)}>`);
-    }
-    const lastVersion = data[0].name.replace(/^v/, '');
+    // if (!Array.isArray(data)) {
+    //   throw new BadRequestException(`Invalid data from Github <${JSON.stringify(data)}>`);
+    // }
+    const lastVersion = data.tag_name.replace(/^v/, '');
     const pkgInfo = this.appService.getInfo();
     const currentVersion = current || pkgInfo.version;
 

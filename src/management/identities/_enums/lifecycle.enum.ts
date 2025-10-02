7024157805cc4f8d9823cef0dd30a2e2b7ec7488
Tacx <12997062+tacxou@users.noreply.github.com>
2025-10-02 12:47:30 +0200

    Refactor IdentityLifecycleState interface: separate definition from default list and ensure color property is included

--- src/management/identities/_enums/lifecycle.enum.ts
+++ src/management/identities/_enums/lifecycle.enum.ts
@@ -4,23 +4,34 @@ export enum IdentityLifecycleDefault {
   MANUAL = "M",
 }
 
-export const IdentityLifecycleDefaultList: Array<{ key: string; label: string; description: string, icon: string }> = [
+export interface IdentityLifecycleState {
+  key: string;
+  label: string;
+  description?: string;
+  icon?: string;
+  color?: string;
+}
+
+export const IdentityLifecycleDefaultList: Array<IdentityLifecycleState> = [
   {
     key: IdentityLifecycleDefault.OFFICIAL,
     label: 'Officiel',
     description: 'supannRessourceEtat : {COMPTE} O SupannActif',
     icon: 'mdi-account-check',
+    color: '#00FF00',
   },
   {
     key: IdentityLifecycleDefault.INACTIVE,
     label: 'Inactif',
     description: 'supannRessourceEtat : {COMPTE} I SupannInactif',
     icon: 'mdi-account-off',
+    color: '#808080',
   },
   {
     key: IdentityLifecycleDefault.MANUAL,
     label: 'Manuel',
     description: 'supannRessourceEtat : {COMPTE} M SupannManuel',
     icon: 'mdi-account-cog',
+    color: '#ff009dff',
   },
 ];

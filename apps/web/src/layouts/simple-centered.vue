f609e20de4e398831879a7148584c4c8ed3c84a4
Tacx <12997062+tacxou@users.noreply.github.com>
2026-02-12 17:34:19 +0100

    feat: améliorer l'interface de connexion avec un nouveau design et ajouter des fonctionnalités de chargement

--- apps/web/src/layouts/simple-centered.vue
+++ apps/web/src/layouts/simple-centered.vue
@@ -5,13 +5,61 @@ q-layout(view="lHh Lpr fff")
       :style-fn="getStyle"
     )
       nuxt-page
+      q-fab.absolute(
+        style="bottom: 16px; right: 16px;"
+        color="primary"
+        icon="mdi-help-circle"
+        direction='left'
+      )
+        q-fab-action(
+          color="primary"
+          icon='mdi-theme-light-dark' @click="toogleDark()"
+        )
+        q-fab-action(
+          color="primary"
+          icon='mdi-frequently-asked-questions'
+          @click="openLink()"
+        )
 </template>
 
-<script lang="ts" setup>
-const getStyle = () => {
-  return {
-    backgroundImage: `url('/config/login-background.png'), linear-gradient(#5274C5, #9A4A9F)`,
-    backgroundSize: 'cover',
-  }
-}
+<script lang="ts">
+export default defineNuxtComponent({
+  name: 'SimpleCenteredLayout',
+  setup() {
+    const getStyle = () => {
+      return {
+        backgroundImage: `url('/config/login-background.png'), linear-gradient(#5274C5, #9A4A9F)`,
+        backgroundSize: 'cover',
+      }
+    }
+
+    return { getStyle }
+  },
+  computed: {
+    getQAndALink(): string {
+      return (
+        'https://github.com/Libertech-FR/sesame-orchestrator/discussions/new?' +
+        [
+          'category=q-a',
+          'title=[QUESTION]%20Votre%20titre%20ici',
+          "body=Décrivez%20votre%20question%20ici.%0A%0A---%0A%0A*Merci%20de%20ne%20pas%20oublier%20de%20fournir%20les%20informations%20suivantes%20:%0A%20-%20Version%20de%20l'Orchestrator%20:%20" +
+            'N/A' +
+            '%0A%20-%20Version%20du%20Daemon%20:%20' +
+            'N/A' +
+            "%0A%20-%20Système%20d'exploitation%20:%20" +
+            (navigator.userAgent || 'N/A'),
+        ].join('&')
+      )
+    },
+  },
+  methods: {
+    openLink() {
+      window.open(this.getQAndALink, '_blank')
+    },
+    toogleDark() {
+      this.$q.dark.toggle()
+      window.sessionStorage.setItem('darkMode', this.$q.dark.isActive ? 'true' : 'false')
+    },
+  },
+})
 </script>
--- apps/web/src/pages/login.vue
+++ apps/web/src/pages/login.vue
@@ -1,23 +1,93 @@
 <template lang="pug">
-q-card.col-4.shadow-24.row(style="max-width: 1200px;")
+q-card.col.q-ma-xl(flat bordered style="min-width: 300px; max-width: 600px;")
   q-toolbar.bg-primary.text-white
-    q-toolbar-title Connexion
-  q-card-section.col-4.flex.items-center
-    q-img(src="/config/login-side.png" error-src="/default.png" alt="Ecole logo" style="max-width: 100%;")
-  q-card-section.col-8.column
-    form(@submit.prevent='submit')
-      q-card.no-shadow
-        q-card-section.q-pt-sm.q-px-lg.q-pb-lg
-          q-input(v-model="formData.username" label="Username" type="text")
-          q-input(v-model="formData.password" label="Password" type="password" auto-complete='current-password')
-        q-inner-loading(:showing='pending')
-          q-spinner-grid(color='primary' size='50px')
-      q-card-actions.column.justify-between
-        .col.q-my-sm.flex.items-center
-          q-btn(@click.prevent='submit' type='submit' color='primary' size="lg" ) Se connecter
+    q-toolbar-title Authentification
+  div(:class='[$q.screen.gt.xs ? "row" : "column"]')
+    q-card-section.col-5(v-if='$q.screen.gt.xs')
+      q-img.full-height(
+        src="/config/login-side.png"
+        error-src="/default.png"
+        alt="Sesame logo"
+        fit="cover"
+      )
+    q-separator.q-mx-md.q-my-md(v-if='$q.screen.gt.xs' vertical)
+    q-card.col(flat)
+      form.fit(@submit.prevent='submit')
+        q-card-section.column.full-height
+          .col.column.items-center.justify-center.q-gutter-md.q-pl-md
+            q-input.full-width(
+              v-model="formData.username"
+              label="Nom d'utilisateur"
+              type="text"
+              :readonly='pending'
+              autocomplete='username'
+              outlined
+            )
+            q-input.full-width(
+              v-model="formData.password"
+              label="Mot de passe"
+              type="password"
+              autocomplete='current-password'
+              :readonly='pending'
+              outlined
+            )
+          q-card-actions.q-mt-md(
+            stzyle='position: sticky; bottom: 0; right: 0; left: 0;'
+          )
+            q-space
+            q-btn(
+              @click.prevent='submit'
+              type='submit'
+              color='primary'
+              size="lg"
+              :loading='pending'
+            ) Se connecter
+            q-space
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+export default defineNuxtComponent({
+  name: 'LoginPage',
+  setup() {
+    definePageMeta({
+      layout: 'simple-centered',
+      auth: {
+        unauthenticatedOnly: true,
+        navigateAuthenticatedTo: '/',
+      },
+    })
+
+    return {
+      pending: ref(false),
+      formData: ref({
+        username: '',
+        password: '',
+      }),
+    }
+  },
+  methods: {
+    async submit() {
+      this.pending = true
+      try {
+        await useAuth().loginWith('local', {
+          body: this.formData,
+        })
+      } catch (error) {
+        this.$q.notify({
+          type: 'negative',
+          message: 'Erreur de connexion: ' + (error instanceof Error ? error.message : 'Unknown error'),
+          position: 'top',
+          timeout: 10_000,
+        })
+      } finally {
+        this.pending = false
+      }
+    },
+  },
+})
+</script>
+
+<!-- <script lang="ts" setup>
 import { ref } from 'vue'
 import { useQuasar } from 'quasar'
 definePageMeta({
@@ -38,9 +108,13 @@ const formData = ref({
 const submit = async () => {
   pending.value = true
   try {
-    await useAuth().loginWith('local', {
-      body: formData.value,
-    })
+    await useAuth()
+      .loginWith('local', {
+        body: formData.value,
+      })
+      .catch(() => {
+        throw new Error('Invalid credentials')
+      })
   } catch (error) {
     $q.notify({
       type: 'negative',
@@ -50,4 +124,11 @@ const submit = async () => {
     pending.value = false
   }
 }
-</script>
+</script> -->
+
+<style lang="sass" scoped>
+::v-deep body.body--dark input:-webkit-autofill
+  -webkit-box-shadow: 0 0 0px 1000px var(--q-dark) inset
+  -webkit-text-fill-color: #fff !important
+  caret-color: #fff !important
+</style>

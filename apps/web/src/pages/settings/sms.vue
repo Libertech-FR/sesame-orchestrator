07ff007eb291f0de4a8e83ce55cc45907d12f196
Tacx <12997062+tacxou@users.noreply.github.com>
2026-02-23 17:32:44 +0100

    feat: refactor la page des paramètres SMS pour utiliser un modèle de données et améliorer la validation des champs

--- apps/web/src/pages/settings/sms.vue
+++ apps/web/src/pages/settings/sms.vue
@@ -1,106 +1,150 @@
-<template>
-  <div>
-  <div class="row q-gutter-lg">
-    <h5>Serveur SMPP (envoi des Sms)</h5>
-  </div>
-  <div class="q-pa-md q-gutter-lg">
-    <div class="row">
-      <q-input style="width:50%" type="text" outlined v-model="host"   label="Serveur SMPP (URL smpp://...) " dense/>
-    </div>
-    <div class="row">
-      <q-input style="width:50%" type="text" outlined v-model="systemId"   label="System ID" dense/>
-    </div>
-    <div class="row">
-      <q-input style="width:50%" :type="typePasswordProp" outlined v-model="password"   label="Mot de passe" dense>
-        <template v-slot:append>
-          <q-icon name="mdi-eye" @click="togglePassword" style="cursor: pointer;"/>
-        </template>
-      </q-input>
-    </div>
-    <div class="row">
-      <q-input style="width:50%" type="text" outlined v-model="sourceAddr"   label="Nom ou numéro de l'emetteur" dense/>
-    </div>
-    <div class="q-pa-md q-gutter-sm fixed-bottom">
-      <q-btn color="primary" style="width: 100%" @click="saveParams">
-        <div class="ellipsis">
-          Sauvegarder les parametres
-        </div>
-      </q-btn>
-    </div>
-  </div>
-  </div>
+<template lang="pug">
+.sesame-page
+  .sesame-page-content.q-pa-md.flex
+    div.fit
+      .row.q-gutter-lg
+        h5.q-ma-lg
+          span Serveur SMPP&nbsp;
+          small (envoi des SMS)
+      .row.q-col-gutter-md
+        q-input.col-12.col-md-6(
+          type="text"
+          outlined
+          v-model="payload.host"
+          label="Serveur SMPP (URL smpp://...)"
+          hint="Exemple : smpp://smpp.server.com:port"
+          :error="!!validations['host']"
+          :error-message='validations["host"]'
+          dense
+        )
+        .offset-md-1
+        q-input.col-12.col-md-6(
+          type="text"
+          outlined
+          v-model="payload.systemId"
+          label="System ID"
+          hint="Identifiant de connexion au serveur SMPP"
+          :error="!!validations['systemId']"
+          :error-message='validations["systemId"]'
+          dense
+        )
+        .offset-md-1
+        q-input.col-12.col-md-6(
+          :type="typePasswordProp"
+          outlined
+          v-model="payload.password"
+          label="Mot de passe"
+          hint="Mot de passe de connexion au serveur SMPP"
+          :error="!!validations['password']"
+          :error-message='validations["password"]'
+          dense
+        )
+          template(v-slot:append)
+            q-icon.cursor-pointer(name="mdi-eye" @click="togglePassword")
+        .offset-md-1
+        q-input.col-12.col-md-6(
+          type="text"
+          outlined
+          v-model="payload.sourceAddr"
+          label="Nom ou numéro de l'emetteur"
+          hint="Nom ou numéro qui apparaîtra comme expéditeur des SMS"
+          :error="!!validations['sourceAddr']"
+          :error-message='validations["sourceAddr"]'
+          dense
+        )
+        .offset-md-1
+  q-card-actions.sticky-footer.border-top.full-width
+    q-space
+    q-btn.text-positive(
+      flat
+      label="Sauvegarder les paramètres"
+      icon-right="mdi-content-save"
+      @click="saveParams"
+    )
 </template>
 
-<script setup>
+<script lang="ts">
+import { ref } from 'vue'
 
-import {onMounted, ref} from "vue";
-import {useQuasar} from "quasar";
-const { handleError } = useErrorHandling()
-const $q=useQuasar()
-const host=ref('')
-const systemId=ref('')
-const password=ref('')
-const regionCode=ref('FR')
-const sourceAddr=ref('')
-const typePasswordProp=ref('password')
-onMounted(() => {
-  readParams()
+type SmsSettings = {
+  host: string
+  systemId: string
+  password: string
+  regionCode: string
+  sourceAddr: string
+}
+
+export default defineComponent({
+  name: 'SettingsSmsPage',
+  data() {
+    return {
+      typePasswordProp: 'password',
+    }
+  },
+  async setup() {
+    const { handleError } = useErrorHandling()
+
+    const payload = ref({
+      host: '',
+      systemId: '',
+      password: '',
+      regionCode: 'FR',
+      sourceAddr: '',
+    })
+    const validations = ref({} as Record<string, any>)
+
+    const {
+      data: result,
+      pending,
+      error,
+      refresh,
+    } = await useHttp<{ data: SmsSettings }>(`/settings/sms/get`, {
+      method: 'GET',
+    })
+    if (error.value) {
+      handleError({
+        error: error.value,
+        message: 'Erreur lors de de la lecture des paramètres',
+      })
+    } else {
+      payload.value = result.value?.data || payload.value
+      validations.value = {}
+    }
+
+    return {
+      payload,
+      handleError,
+      pending,
+      refresh,
+      validations,
+    }
+  },
+  methods: {
+    async saveParams() {
+      try {
+        await this.$http.post(`/settings/sms/set`, {
+          body: this.payload,
+        })
+
+        this.$q.notify({
+          message: 'Les paramètres ont été sauvegardés',
+          color: 'positive',
+          position: 'top-right',
+          icon: 'mdi-check-circle-outline',
+        })
+        this.validations = {}
+      } catch (error: any) {
+        this.handleError({
+          error,
+          message: error?.response?._data?.message || 'Erreur lors de la sauvegarde des paramètres',
+          notify: true,
+        })
+        this.validations = error?.response?._data?.validations || {}
+      }
+    },
+    togglePassword() {
+      this.typePasswordProp = this.typePasswordProp === 'password' ? 'text' : 'password'
+    },
+  },
 })
-async function readParams(){
-  console.log('readParams')
-  const { data: result, pending, error, refresh } = await useHttp(`/settings/sms/get`, {
-    method: 'GET',
-  });
-  if (error.value) {
-    handleError({
-      error: error.value,
-      message: 'Erreur lors de de la lecture des paramètres'
-    })
-  } else {
-    host.value=result.value.data.host
-    systemId.value=result.value.data.systemId
-    password.value=result.value.data.password
-    regionCode.value=result.value.data.regionCode
-    sourceAddr.value=result.value.data.sourceAddr
-  }
-}
-async function saveParams() {
-  const data = {
-    host: host.value,
-    systemId: systemId.value,
-    regionCode: regionCode.value,
-    password: password.value,
-    sourceAddr:sourceAddr.value
-  }
-  const {data: result, pending, error, refresh} = await useHttp(`/settings/sms/set`, {
-    method: 'POST',
-    body: data
-  });
-  if (error.value) {
-    handleError({
-      error: error.value,
-      message: 'Erreur lors de la sauvegarde des parametres',
-      notify: true
-    })
-
-  } else {
-    $q.notify({
-      message: 'Les parametres ont été sauvegardés',
-      color: 'positive',
-      position: 'top-right',
-      icon: 'mdi-check-circle-outline',
-    })
-  }
-}
-function togglePassword(){
-  if (typePasswordProp.value === 'password'){
-    typePasswordProp.value='text'
-  }else{
-    typePasswordProp.value='password'
-  }
-}
 </script>
-
-<style scoped>
-
-</style>

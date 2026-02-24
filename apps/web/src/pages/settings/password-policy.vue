cff4f1dc946fc184a182ddb4dc146b89bb78ccd7
Tacx <12997062+tacxou@users.noreply.github.com>
2026-02-24 16:10:51 +0100

    feat: refactor la page de politique de mots de passe pour améliorer la structure et l'accessibilité des paramètres

--- apps/web/src/pages/settings/password-policy.vue
+++ apps/web/src/pages/settings/password-policy.vue
@@ -1,174 +1,234 @@
-<template>
-  <div>
-    <div class="row q-gutter-sm">
-      <h5>Politique des mots de passe</h5>
-    </div>
-    <div class="q-pa-md q-gutter-md">
-      <div class="row">
-        <div class="col-4">
-          <q-input type="number" outlined v-model="len" input-class="text-right" label="Longueur minimale " dense> </q-input>
-        </div>
-        <div class="col-4">
-          <q-input type="number" outlined v-model="minComplexity" input-class="text-right" label="Entropie minimale " dense> </q-input>
-        </div>
-        <div class="col-4">
-          <q-input type="number" outlined v-model="goodComplexity" input-class="text-right" label="Entropie bonne " dense> </q-input>
-        </div>
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="hasUpper" color="green" label="Doit contenir au moins une Majuscule" />
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="hasLower" color="blue" label="Doit contenir au moins une Minuscule" />
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="hasNumber" color="orange" label="Doit contenir au moins un chiffre" />
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="hasSpecialChars" color="blue" label="Doit contenir au moins un caractère special" />
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="checkPwned" color="black" label="Vérifier si le mot de pass est connu avec pwned " />
-      </div>
-      <div row="row">
-        <q-toggle dense v-model="smsEnabled" color="red" label="Reinitialisation par SMS active" />
-      </div>
-      <div class="row">
-        <q-input style="width: 50%" type="text" outlined v-model="mailAttribute" label="Attribut de l'identité contenant le mail alternatif " dense />
-      </div>
-      <div class="row">
-        <q-input style="width: 50%" type="text" outlined v-model="mobileAttribute" label="Attribut de l'identité contenant le numero de mobile " dense />
-      </div>
-      <div class="row">
-        <q-input style="width: 50%" type="url" outlined v-model="redirectUrl" label="Url de redirection après un changement de mot de passe " dense />
-      </div>
-      <div class="row">
-        <q-input style="width: 30%" type="number" outlined v-model="resetCodeTTL" label="Temps de vie du code de reninitialisation du code (en secondes)" dense />
-      </div>
-      <div class="row">
-        <q-input style="width: 30%" type="number" outlined v-model="initTokenTTL" label="Temps de vie du mail d'initialisation  (en secondes)" dense />
-      </div>
-      <div class="q-pa-md q-gutter-sm fixed-bottom">
-        <q-btn color="primary" style="width: 100%" @click="saveParams">
-          <div class="ellipsis">Sauvegarder les parametres</div>
-        </q-btn>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.sesame-page
+  .sesame-page-content.q-pa-md.flex
+    div.fit
+      .row.q-gutter-lg
+        h5.q-ma-lg
+          span Politique des mots de passe&nbsp;
+      .row.q-col-gutter-md
+        q-input.col-12.col-sm-4.col-md-3.col-lg-3(
+          type="number"
+          outlined
+          v-model="payload.len"
+          input-class="text-right"
+          label="Longueur minimale"
+          hint="Longueur minimale du mot de passe"
+          dense
+        )
+        q-input.col-12.col-sm-4.col-md-3.col-lg-3(
+          type="number"
+          outlined
+          v-model="payload.minComplexity"
+          input-class="text-right"
+          label="Entropie minimale"
+          hint="Entropie à atteindre pour que le mot de passe soit considéré comme acceptable"
+          dense
+        )
+        q-input.col-12.col-sm-4.col-md-3.col-lg-3(
+          type="number"
+          outlined
+          v-model="payload.goodComplexity"
+          input-class="text-right"
+          label="Complexité souhaitée"
+          hint="Entropie à atteindre pour que le mot de passe soit considéré comme bon"
+          dense
+        )
+      q-separator.q-mt-xl
+      .row.q-col-gutter-md.q-mt-md
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.hasUpperCase"
+          color="green"
+          label="Doit contenir au moins une Majuscule"
+          :true-value="1"
+          :false-value="0"
+        )
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.hasLowerCase"
+          color="purple"
+          label="Doit contenir au moins une Minuscule"
+          :true-value="1"
+          :false-value="0"
+        )
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.hasNumbers"
+          color="orange"
+          label="Doit contenir au moins un chiffre"
+          :true-value="1"
+          :false-value="0"
+        )
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.hasSpecialChars"
+          color="blue"
+          label="Doit contenir au moins un caractère spécial"
+          :true-value="1"
+          :false-value="0"
+        )
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.checkPwned"
+          color="black"
+          label="Vérifier les mots de passe compromis"
+          hint="Utilise l'API de pwned pour vérifier si le mot de passe a été compromis dans une fuite de données"
+        )
+        q-toggle.col-12.col-sm-6.col-md-4.col-lg-3(
+          dense
+          v-model="payload.resetBySms"
+          color="red"
+          label="Réinitialisation par SMS active"
+        )
+      q-separator.q-mt-lg
+      .row.q-col-gutter-md.q-mt-md
+        q-input.col-12.col-sm-6.col-md-5.col-lg-4(
+          type="text"
+          outlined
+          v-model="payload.emailAttribute"
+          label="Attribut de l'identité contenant le mail alternatif"
+          hint="Attribut de l'identité contenant le mail alternatif utilisé pour la réinitialisation du mot de passe"
+          dense
+        )
+        q-input.col-12.col-sm-6.col-md-5.col-lg-4(
+          type="text"
+          outlined
+          v-model="payload.mobileAttribute"
+          label="Attribut de l'identité contenant le numéro de mobile"
+          hint="Attribut de l'identité contenant le numéro de mobile utilisé pour la réinitialisation du mot de passe"
+          dense
+        )
+        q-input.col-12.col-md-10.col-lg-8(
+          type="url"
+          outlined
+          v-model="payload.redirectUrl"
+          label="URL de redirection après changement de mot de passe"
+          hint="URL de redirection après un changement de mot de passe réussi"
+          dense
+        )
+      q-separator.q-my-lg
+      .row.q-col-gutter-md
+        q-input.col-12.col-md-6(
+          type="number"
+          outlined
+          v-model="payload.resetCodeTTL"
+          label="Temps de vie du code de réinitialisation (en secondes)"
+          hint="Durée pendant laquelle le code de réinitialisation est valide"
+          dense
+        )
+        q-input.col-12.col-md-6(
+          type="number"
+          outlined
+          v-model="payload.initTokenTTL"
+          label="Temps de vie du token d'initialisation (en secondes)"
+          hint="Durée pendant laquelle le token d'initialisation envoyé par mail est valide"
+          dense
+        )
+  q-card-actions.sticky-footer.border-top.full-width
+    q-space
+    q-btn.text-positive(
+      flat
+      label="Sauvegarder les paramètres"
+      icon-right="mdi-content-save"
+      @click="saveParams"
+    )
 </template>
 
-<script setup>
-import { onMounted, ref } from 'vue'
-import { useQuasar } from 'quasar'
-const $q = useQuasar()
-const len = ref(8)
-const hasUpper = ref(false)
-const hasLower = ref(false)
-const hasNumber = ref(false)
-const hasSpecialChars = ref(false)
-const checkPwned = ref(false)
-const smsEnabled = ref(false)
-const mailAttribute = ref('')
-const mobileAttribute = ref('')
-const redirectUrl = ref('')
-const goodComplexity = ref(0)
-const minComplexity = ref(0)
-const resetCodeTTL = ref(0)
-const initTokenTTL = ref(0)
+<script lang="ts">
+import { ref } from 'vue'
 
-// const data = ref({
-//   checkPwned: true,
-//   emailAttribute: 'inetOrgPerson.mail',
-//   goodComplexity: 70,
-//   hasLowerCase: 1,
-//   hasNumbers: 1,
-//   hasSpecialChars: 1,
-//   hasUpperCase: 1,
-//   initTokenTTL: 604800,
-//   len: 12,
-//   minComplexity: 30,
-//   mobileAttribute: '',
-//   redirectUrl: '',
-//   resetBySms: false,
-//   resetCodeTTL: 900,
-// })
+type PasswordPolicySettings = {
+  len: number
+  hasUpperCase: 0 | 1
+  hasLowerCase: 0 | 1
+  hasNumbers: 0 | 1
+  hasSpecialChars: 0 | 1
+  checkPwned: boolean
+  resetBySms: boolean
+  emailAttribute: string
+  mobileAttribute: string
+  redirectUrl: string
+  goodComplexity: number
+  minComplexity: number
+  resetCodeTTL: number
+  initTokenTTL: number
+}
 
-onMounted(() => {
-  readParams()
+export default defineComponent({
+  name: 'SettingsPasswordPolicyPage',
+  data() {
+    return {}
+  },
+  async setup() {
+    const { handleError } = useErrorHandling()
+
+    const payload = ref({
+      len: 8,
+      hasUpperCase: 0,
+      hasLowerCase: 0,
+      hasNumbers: 0,
+      hasSpecialChars: 0,
+      checkPwned: false,
+      resetBySms: false,
+      emailAttribute: '',
+      mobileAttribute: '',
+      redirectUrl: '',
+      goodComplexity: 0,
+      minComplexity: 0,
+      resetCodeTTL: 0,
+      initTokenTTL: 0,
+    })
+    const validations = ref({} as Record<string, any>)
+
+    const {
+      data: result,
+      pending,
+      error,
+      refresh,
+    } = await useHttp<{ data: PasswordPolicySettings }>(`/settings/passwdadm/getpolicies`, {
+      method: 'GET',
+    })
+    if (error.value) {
+      handleError({
+        error: error.value,
+        message: 'Erreur lors de de la lecture des paramètres',
+      })
+    } else {
+      payload.value = result.value?.data || payload.value
+      validations.value = {}
+    }
+
+    return {
+      payload,
+      handleError,
+      pending,
+      refresh,
+      validations,
+    }
+  },
+  methods: {
+    async saveParams() {
+      try {
+        await this.$http.post(`/settings/passwdadm/setpolicies`, {
+          body: this.payload,
+        })
+
+        this.$q.notify({
+          message: 'Les paramètres ont été sauvegardés',
+          color: 'positive',
+          position: 'top-right',
+          icon: 'mdi-check-circle-outline',
+        })
+        this.validations = {}
+      } catch (error: any) {
+        this.handleError({
+          error,
+          message: error?.response?._data?.message || 'Erreur lors de la sauvegarde des paramètres',
+          notify: true,
+        })
+        this.validations = error?.response?._data?.validations || {}
+      }
+    },
+  },
 })
-
-async function readParams() {
-  console.log('readParams')
-  const {
-    data: result,
-    pending,
-    error,
-    refresh,
-  } = await useHttp(`/settings/passwdadm/getpolicies`, {
-    method: 'GET',
-  })
-  if (error.value) {
-    handleError({
-      error: error.value,
-      message: 'Erreur lors de de la lecture des police',
-    })
-  } else {
-    redirectUrl.value = result.value.data.redirectUrl
-    len.value = result.value.data.len
-    hasUpper.value = result.value.data.hasUpperCase === 1
-    hasLower.value = result.value.data.hasLowerCase === 1
-    hasNumber.value = result.value.data.hasNumbers === 1
-    hasSpecialChars.value = result.value.data.hasSpecialChars === 1
-    smsEnabled.value = result.value.data.resetBySms
-    checkPwned.value = result.value.data.checkPwned
-    minComplexity.value = result.value.data.minComplexity
-    goodComplexity.value = result.value.data.goodComplexity
-    mobileAttribute.value = result.value.data.mobileAttribute
-    mailAttribute.value = result.value.data.emailAttribute
-    resetCodeTTL.value = result.value.data.resetCodeTTL
-    initTokenTTL.value = result.value.data.initTokenTTL
-  }
-}
-async function saveParams() {
-  const data = {
-    len: len.value,
-    hasUpperCase: hasUpper.value === true ? 1 : 0,
-    hasLowerCase: hasLower.value === true ? 1 : 0,
-    hasNumbers: hasNumber.value === true ? 1 : 0,
-    hasSpecialChars: hasSpecialChars.value === true ? 1 : 0,
-    minComplexity: minComplexity.value,
-    goodComplexity: goodComplexity.value,
-    checkPwned: checkPwned.value,
-    resetBySms: smsEnabled.value,
-    redirectUrl: redirectUrl.value,
-    emailAttribute: mailAttribute.value,
-    mobileAttribute: mobileAttribute.value,
-    resetCodeTTL: resetCodeTTL.value,
-    initTokenTTL: initTokenTTL.value,
-  }
-  const {
-    data: result,
-    pending,
-    error,
-    refresh,
-  } = await useHttp(`/settings/passwdadm/setpolicies`, {
-    method: 'POST',
-    body: data,
-  })
-  if (error.value) {
-    handleError({
-      error: error.value,
-      message: 'Erreur lors de la sauvegarde des parametres',
-    })
-  } else {
-    $q.notify({
-      message: 'Les parametres ont été sauvegardés',
-      color: 'positive',
-      position: 'top-right',
-      icon: 'mdi-check-circle-outline',
-    })
-  }
-}
 </script>
-
-<style scoped></style>
